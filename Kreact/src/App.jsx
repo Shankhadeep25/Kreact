923c455abed33cd757cf41c92cbbb32e1d39265c
Shankhadeep25 <shankhadeepdey251@gmail.com>
2026-02-19 15:03:16 +0530

    Outlet added

--- Kreact/src/App.jsx
+++ Kreact/src/App.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './NavBar';
+import Body from './Body';
+import {BrowserRouter,
+  Route,Routes
+} from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <NavBar/>
-      <h1 className="text-3xl font-bold">Hello World</h1>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body/>}>
+            <Route path="/login" element={<div>login Page</div>}/>
+            <Route path="/test" element={<div>test Page</div>}/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      {/* <h1 className="text-3xl font-bold">Hello World</h1> */}
     </>
   );
 }
