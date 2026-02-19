b8776e9e46caf97d867af3fcc12a5624082d5339
Shankhadeep25 <shankhadeepdey251@gmail.com>
2026-02-20 00:49:48 +0530

    if token not present redirect to login page

--- Kreact/src/App.jsx
+++ Kreact/src/App.jsx
@@ -17,9 +17,9 @@ function App() {
         <BrowserRouter basename="/">
           <Routes>
             <Route path="/" element={<Body />}>
-              <Route path="/" element={<Feed />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/test" element={<Profile />} />
+              <Route index element={<Feed />} />
+              <Route path="login" element={<Login />} />
+              <Route path="profile" element={<Profile />} />
             </Route>
           </Routes>
         </BrowserRouter>
