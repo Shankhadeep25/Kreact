06f341b17f8d862662b18db7d52f989d4f9fcb8e
Shankhadeep25 <shankhadeepdey251@gmail.com>
2026-02-20 20:24:25 +0530

    UserCard Added

--- Kreact/src/components/Feed.jsx
+++ Kreact/src/components/Feed.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import axios from "axios"
+import { BASE_URL } from "../utils/constants"
+import { useDispatch, useSelector } from "react-redux"
+import { addFeed } from "../utils/feedSlice"
+import { useEffect } from "react"
+import UserCard from "./UserCard"
 
 const Feed = () => {
+  const feed = useSelector((store) => store.feed)
+  const dispatch = useDispatch()
+
+  const getFeed = async () => {
+    if (feed) return;
+    try {
+      const res = await axios.get(BASE_URL + "/user/feed", {
+        withCredentials: true,
+      });
+      dispatch(addFeed(res.data))
+    }
+    catch (err) {
+      console.error(
+    err.response?.data?.message || 
+    err.message || 
+    "Something went wrong")
+    }
+
+  }
+
+      useEffect(() => {
+        getFeed();
+      }, []);
+
   return (
-    <div>Feed</div>
+    feed && (
+    <div className="flex justify-center my-10">
+      <UserCard user={feed[0]}/>
+    </div>
+    )
   )
 }
 
