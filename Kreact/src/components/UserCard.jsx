d9e56483a5982afc8025c3d4824886c8e47dec88
Shankhadeep25 <shankhadeepdey251@gmail.com>
2026-02-20 23:11:06 +0530

    Profile page made

--- Kreact/src/components/UserCard.jsx
+++ Kreact/src/components/UserCard.jsx
@@ -1,11 +1,11 @@
 import { use } from "react";
 
 const UserCard = ({user}) => {
-    const {firstName, lastName, photourl, age, gender, about} = user
+    const {firstName, lastName, photoUrl, age, gender, about} = user
   return (
     <div className="card bg-base-300 w-96 shadow-sm">
       <figure>
-        <img src={user.photourl} alt="PhotoUrl" />
+        <img src={photoUrl} alt="PhotoUrl" />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{firstName + " " + lastName}</h2>
